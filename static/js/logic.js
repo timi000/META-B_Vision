305c8d30499a0e5d2c8a135b66cde56b4c8df411
Maury Agami <mauricioagami@live.com>
2020-09-22 19:42:07 -0400

    added home page, moved back chrorpleth page, added minor changes to css

---
+++ static/js/logic.js
@@ -0,0 +1,50 @@
+var mapboxAccessToken = apiKey;
+var map = L.map('map').setView([56.1304, -106.3468], 8);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + mapboxAccessToken, {
+    id: 'light-v9',
+    attribution: ...,
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(map);
+
+
+function getColor(d) {
+    return d > 1000 ? '#800026' :
+           d > 500  ? '#BD0026' :
+           d > 200  ? '#E31A1C' :
+           d > 100  ? '#FC4E2A' :
+           d > 50   ? '#FD8D3C' :
+           d > 20   ? '#FEB24C' :
+           d > 10   ? '#FED976' :
+                      '#FFEDA0';
+};
+
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.density),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+L.geoJson(/*insert canada file here*/, {style: style}).addTo(map);
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+        
+    });
+
+}
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+}
